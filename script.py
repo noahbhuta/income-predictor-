6ac0b6a4bbd530efb8a39a4eca4f8b3adf964031
noahbhuta <37279148+noahbhuta@users.noreply.github.com>
2022-06-16 11:21:13 -0400

    Create script.py

---
+++ script.py
@@ -0,0 +1,26 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn import tree
+from sklearn.ensemble import RandomForestClassifier
+
+income_data = pd.read_csv('income.csv',header = 0,delimiter = ", ")
+
+print(income_data.iloc[0])
+
+income_data["sex"] = income_data['sex'].replace({'Female':1, 'Male':0})
+
+income_data["native-country"] = income_data["native-country"].apply(lambda row: 1 if row == "United-States" else 0)
+
+income_data["workclass"] = income_data["workclass"].apply(lambda row: 1 if row == "Without-pay" or row == 'Never-worked ' else 0)
+
+labels = income_data[['income']]
+data = income_data[["age", "capital-gain", "capital-loss", "hours-per-week",'sex',"education-num","workclass"]]
+
+train_data, test_data, train_labels, test_labels = train_test_split(data,labels,random_state = 1)
+
+forest = RandomForestClassifier(random_state = 1)
+forest.fit(train_data,train_labels)
+print(forest.feature_importances_)
+print(forest.score(test_data,test_labels))
+
+print(income_data["workclass"].value_counts())
